a3ef43be7fd9367bc4090ee0f8491b00ae3ccd0d
David Martínez Tapia <dmmtapia.ux@gmail.com>
2024-03-06 19:44:38 -0300

    orden SQL

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -25,15 +25,15 @@ export const getAllAdmin = async (req, res) => {
   }
 };
 
-
-
-
 /* //todo---------------------------------------------------------------->  Ejercicio 2 <-------------------------------------------------------------- 
  Arreglo con los nombres de cada tipo de cultivo, ordenados decrecientemente por la suma TOTAL de la cantidad de hectáreas plantadas de cada uno de ellos.*/
 export const getTipoCuartel = async (req, res) => {
   try {
     const { rows, rowCount } = await db.query(
-      "SELECT e.name, ROUND(SUM(qs.area/10000),3) AS hectarea FROM tipo_cuartel e JOIN cuarteles qs ON id_paddock_type = e.id GROUP BY e.id, e.name ORDER BY hectarea DESC"
+      `SELECT tc.name, ROUND(SUM(qs.area/10000),3) AS hectarea 
+        FROM tipo_cuartel tc 
+        JOIN cuarteles qs ON id_paddock_type = tc.id GROUP BY tc.id, tc.name 
+        ORDER BY hectarea DESC`
     );
     console.log("Tipo de cultivo y cantidad de hectareas", rows, rowCount);
     if (!rows.length)
@@ -54,15 +54,20 @@ export const getTipoCuartel = async (req, res) => {
   }
 };
 
-
-
-
 /* //todo---------------------------------------------------------------->  Ejercicio 3 <-------------------------------------------------------------- 
 Arreglo con nombres de los administradores de la FORESTAL Y AGRÍCOLA LO ENCINA, ordenados por nombre, que trabajen más de 1000 m2 de Cerezas */
 export const getAdminForestalAgricola = async (req, res) => {
   try {
     const { rows, rowCount } = await db.query(
-      "SELECT a.name, SUM(c.area) AS area_total_cerezas FROM administradores a JOIN cuarteles c ON a.id = c.id_admin JOIN granjas g ON c.id_farm = g.id JOIN tipo_cuartel tc ON c.id_paddock_type = tc.id WHERE g.name = 'FORESTAL Y AGRICOLA LO ENCINA' AND tc.name = 'CEREZAS' AND c.area > 1000 GROUP BY a.id, a.name"  
+      `SELECT a.name, SUM(c.area) AS area_total_cerezas 
+        FROM administradores a 
+        JOIN cuarteles c ON a.id = c.id_admin 
+        JOIN granjas g ON c.id_farm = g.id 
+        JOIN tipo_cuartel tc ON c.id_paddock_type = tc.id 
+        WHERE g.name = 'FORESTAL Y AGRICOLA LO ENCINA' 
+        AND tc.name = 'CEREZAS' 
+        AND c.area > 1000 
+        GROUP BY a.id, a.name`
     );
 
     console.log(`administradores de FORESTAL Y AGRICOLA`, rows);
@@ -95,19 +100,24 @@ export const getAdminForestalAgricola = async (req, res) => {
 export const getYearCultivo = async (req, res) => {
   try {
     const { rows, rowCount } = await db.query(
-      "SELECT CONCAT(tc.name, '-', c.harvest_year) AS tipo_de_cultivo, json_object_agg(a.id, a.name) AS administradores FROM cuarteles c JOIN tipo_cuartel tc ON c.id_paddock_type = tc.id JOIN administradores a ON c.id_admin = a.id GROUP BY tc.name, c.harvest_year;"
+      `SELECT CONCAT(tc.name, '-', c.harvest_year) AS tipo_de_cultivo, json_object_agg(a.id, a.name) AS administradores 
+      FROM cuarteles c 
+      JOIN tipo_cuartel tc ON c.id_paddock_type = tc.id 
+      JOIN administradores a ON c.id_admin = a.id 
+      GROUP BY tc.name, c.harvest_year;`
     );
-    console.log("Tipo de cultivo año mas administrador", rows)
-    if(!rows.length)
-    return res.status(404).json({message: "Error al intentar ontener los datos"})
-  res.status(200).json({
-    tipo: rows,
-    count: rowCount,
-    message: "Datos obtenidos con exito"
-  })
+    console.log("Tipo de cultivo año mas administrador", rows);
+    if (!rows.length)
+      return res
+        .status(404)
+        .json({ message: "Error al intentar ontener los datos" });
+    res.status(200).json({
+      tipo: rows,
+      count: rowCount,
+      message: "Datos obtenidos con exito",
+    });
   } catch (error) {
-    console.log(`Hubo un error al intentar obtener los datos ${error}`)
-    res.status(500).json({message: "Error"})
+    console.log(`Hubo un error al intentar obtener los datos ${error}`);
+    res.status(500).json({ message: "Error" });
   }
-}
-
+};
